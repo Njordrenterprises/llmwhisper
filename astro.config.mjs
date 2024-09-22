cab0f606398e568b00933a9b97613d5e23b5ba34
njordr <njordrenterprises@gmail.com>
2024-09-22 01:05:07 -0500

    global style change

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://llmwhisper.com',
-	integrations: [mdx(), sitemap()],
+    site: 'https://llmwhisper.com',
+    integrations: [mdx(), sitemap(), tailwind()],
 });
