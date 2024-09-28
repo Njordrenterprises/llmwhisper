46cdbd5b86d1e8da7288f1d579c3a6497b203b4b
njordr <njordrenterprises@gmail.com>
2024-09-28 17:02:18 -0500

    Add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,14 @@ import sitemap from '@astrojs/sitemap';
 
 import tailwind from '@astrojs/tailwind';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://llmwhisper.com',
+    output: 'server',
+    adapter: vercel({
+        webAnalytics: { enabled: true }
+    }),
     integrations: [mdx(), sitemap(), tailwind()],
 });
